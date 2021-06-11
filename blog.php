050a00a152af5e6c3ea5ec1045be30c8f91f60a4
VTFT <max.traineau@yahoo.fr>
2021-06-11 09:34:40 +0200

    first upload
    blog.php almost finished
    single.php no style

---
+++ blog.php
@@ -0,0 +1,68 @@
+<!-- ANCHOR header start -->
+<?php get_header();
+// Template Name: Blog
+?>
+
+<!-- ANCHOR main start -->
+<main id="blog">
+    <h2>Blog</h2>
+    <?php
+    $recentPosts = new WP_Query();
+    $recentPosts->query('showposts=3');
+    ?>
+    <section id="articles">
+    <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+        <article>
+        
+            <div class="content">
+                <h3><a href='<?php the_permalink() ?>' rel='bookmark'><?php the_title(); ?></a></h3>            
+                <?php the_post_thumbnail(); ?>
+                <div class=“categorie”><?php the_category(); ?></div>
+                <span><?php the_excerpt(); ?></span>
+            </div>
+
+            <div class="infos">
+                <div class=“auteur”><?php the_author(); ?></div>
+                <span class='date'>
+                    <?php the_time('l, jS F, Y');?>
+                </span>
+            </div>
+
+        </article>
+    <?php endwhile;
+    ?>
+    </section>
+
+    <section id="recent">
+        <?php
+        $recentPosts = new WP_Query();
+        $recentPosts->query('showposts=5');
+        ?>
+        <div id="recentArticles">
+            <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+                <article>
+                
+                    <div class="content">
+                        <h3><a href='<?php the_permalink() ?>' rel='bookmark'><?php the_title(); ?></a></h3>            
+                    </div>
+
+                    <div class="infos">
+                        <div class=“auteur”><?php the_author(); ?></div>
+                        <span class='date'>
+                            <?php the_time('l, jS F, Y');?>
+                        </span>
+                    </div>
+
+                </article>
+            <?php endwhile;
+            ?>
+        </div>
+
+        <a href="http://localhost/labyrinthe/archives/" id="button-archives">Voir les archives</a>
+    </section>
+</main>
+<!-- main end -->
+
+<!-- ANCHOR footer start -->
+<?php get_footer();?>
+<!-- footer end -->
---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+// ajout style et script
+function ajout_scripts() {
+    wp_enqueue_style( "style", get_stylesheet_uri());
+    wp_enqueue_script('jquery');
+}
+add_action("wp_enqueue_scripts", "ajout_scripts");
+
+
+// enregistrer le menu
+register_nav_menus( array("menu" => "Menu principal"));
+
+
+//thumbnails
+function mytheme_post_thumbnails() {
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
+
+// Modifier le nombre de mots des extraits
+function wpm_post_excerpt($length) {
+	return 20;
+}
+add_filter('excerpt_length', 'wpm_post_excerpt');
+
+
+
+// Bouton read more
+
+//Nombres de mots a afficher dans l’aperçu des articles page blog
+function new_excerpt_length($length) {
+    return 10;
+}
+add_filter('excerpt_length', 'new_excerpt_length');
+function new_excerpt_more($more) {
+    global $post;
+    return '… <a href="'. get_permalink($post->ID) . '">' . 'Read More &raquo;' . '</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+//fin
+/* ajouter champ “Extrait” dans les page WP */
+function ajouter_extrait_pages() {
+    add_post_type_support( 'page', 'excerpt' );
+}
+add_action( 'init', 'ajouter_extrait_pages' );
